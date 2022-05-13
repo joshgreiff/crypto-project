71dbcb3e7d8bd04a305bb89155838818d3b9150f
afedoriouk <98120553+afedoriouk@users.noreply.github.com>
2022-05-13 15:12:10 -0400

    Update Question.js

---
+++ models/Question.js
@@ -0,0 +1,37 @@
+var answerOptionsElements;
+var questionRound = 0;
+var quizQuestions  = [
+{
+    question:'What is the name of the tallest mountain in the world?',
+    answers:'Everest, Aconcagua, Denali, Kilimanjaro',
+    correctAnswer:'Everest',
+}
+
+    question:'Which country has the largest population in the world?',
+    answers:'China, India, United States, Indonesia',
+    correctAnswer:'China',
+
+{
+    question:'What is the name of the longest river in Africa?',
+    answers:'Nile, Congo, Ubangi, Zambezi',
+    correctAnswer:'Nile',
+}
+{
+    question:'How many States does the United States consist of?',
+    answers:'49, 50, 51, 52',
+    correctAnswer:'50',
+}
+{
+    question:'What is the capital of the American State of California?',
+    answers:'Sacramento, Los Angeles, San Diego, San Francisco ',
+    correctAnswer:'Sacramento',
+}
+{
+    question:'What planet is closest to Earth? ',
+    answers:' Mercury, Venus, Mars, Jupiter',
+    correctAnswer:' Venus',
+}
+
+
+
+]
