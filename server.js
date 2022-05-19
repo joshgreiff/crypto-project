2e6f4b892a62456bf946095d56d8f897492ad2a7
joshgreiff <79293495+joshgreiff@users.noreply.github.com>
2022-05-19 19:00:45 -0400

    pushing

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const sequelize = require('./config/connection')
 const exphbs = require('express-handlebars')
 const hbs = exphbs.create({})
 const routes = require('./controllers/index')
-const session = require('express-session')
+const session = require('express-session');
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express()
 const PORT = process.env.PORT || 3001
 
@@ -24,13 +24,16 @@ const sess = {
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-        db: sequelize
+      db: sequelize
     })
-}
-
-app.use(session(sess))
+  };
+  
+  app.use(session(sess));
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'))
-})
+sequelize.query('SET FOREIGN_KEY_CHECKS = 0', { raw: true }).then ( function () {
+    sequelize.sync ({ force: false }).then ( function () {
+        app.listen(PORT, () => console.log('Now listening'))
+    });
+});
+
