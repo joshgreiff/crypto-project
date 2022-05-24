ddf03602366282bead4ed58617a8128b485979da
afedoriouk <98120553+afedoriouk@users.noreply.github.com>
2022-05-24 17:28:09 -0400

    Create Quiz-Questions.js

---
+++ models/Quiz-Questions.js
@@ -0,0 +1,43 @@
+var answerOptionsElements;
+var questionRound = 0;
+var quizQestions = [
+    {
+        question:'What is the smallest US state',
+        answers:['Rhode Island', 'Kentucky','Louisiana',' Maine',],
+        correctAnswer:'Rhode Island'
+    },
+    {
+        question:'Where can you find Lake Winnebago?',
+        answers:['Rhode Island', 'Kentucky','Wisconsin',' Maine'],
+        correctAnswer:'Wisconsin'
+    },
+   {
+        question:'Where would you find Mount Rushmore?',
+        answers:['Wisconsin',' Maine', 'South Dakota', 'Maine'],
+        correctAnswer:'South Dakota'
+    },
+    {
+        question:'What is the largest lake in the US?',
+        answers:['Lake Huron', 'Lake Michigan', 'Lake Erie','Lake Superior'],
+        correctAnswer:'Lake Superior'
+    },
+    {
+        question:'What is the longest river in the US?',
+        answers:['Missouri River', 'Mississippi River', 'Yukon River', 'Rio Grande River'],
+        correctAnswer:'Missouri River'
+    },
+   {
+        question:'Where would you find the mountain Mauna Kea?',
+        answers:['Rhode Island', 'Kentucky','Wisconsin', 'Montana'],
+        correctAnswer:'Hawaii'
+    },
+    {
+        question:'How many states have a border with Mexico?',
+        answers:['2', '3','4', '5'],
+        correctAnswer:'4'
+    }
+]
+
+
+
+
