064c06b3aad457585a568e567d9802828f565aad
afedoriouk <98120553+afedoriouk@users.noreply.github.com>
2022-05-24 17:28:46 -0400

    Create Quiz-Questions2.js

---
+++ models/Quiz-Questions2.js
@@ -0,0 +1,43 @@
+var answerOptionsElements;
+var questionRound = 0;
+var quizQestions = [
+    {
+        question:'Neil Armstrong is famous for being the first human to walk upon the moon. In what U.S. state did he last set foot before he walked on the lunar surface',
+        answers:['Florida', 'Ohio','Texas','New York'],
+        correctAnswer:'Ohio'
+    },
+    {
+        question:'Rosa Parks is known for refusing in 1955 to give up her seat on a public bus to a white person, thus sparking a bus boycott by African Americans. In the capital city of what U.S. state did Parks refuse to move?',
+        answers:['Louisiana', 'Alabama','Georgia',' Mississippi'],
+        correctAnswer:'Louisiana'
+    },
+   {
+        question:'Charles Lindbergh was the first person to fly solo across the Atlantic Ocean. He became a media hero in 1927 upon landing in Paris. From what U.S. state did he take off on his world-famous flight?',
+        answers:['New Jersey',' Virginia', ' New York', 'Rhode Island'],
+        correctAnswer:'Rhode Island'
+    },
+    {
+        question:'What year was the infamous San Francisco earthquake?',
+        answers:['1907', '1906','1908','1905'],
+        correctAnswer:'1908'
+    },
+    {
+        question:'What national landmark opened in Washington D.C. in 1922?',
+        answers:['The Washington Monument', 'The Pentagon', 'The Jefferson Memorial', 'The Lincoln Memorial'],
+        correctAnswer:'The Washington Monument'
+    },
+   {
+        question:'Which census declared that US population had reached 100 million?',
+        answers:['1920', '1910','1930', '1940'],
+        correctAnswer:'1920'
+    },
+    {
+        question:'How much did a Model T sell for when it was introduced in 1908?',
+        answers:['$850', '$500','$1000', '$650'],
+        correctAnswer:'$500'
+    }
+]
+
+
+
+
